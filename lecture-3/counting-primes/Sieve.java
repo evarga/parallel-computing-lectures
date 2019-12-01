827e8986e46eaad861ac302c86aa062efb3d6d77
Ervin Varga <e.varga@ieee.org>
2019-12-01 03:52:01 +0100

    Restructure code to use BitSet instead of ordinary boolean array.

--- lecture-3/counting-primes/Sieve.java
+++ lecture-3/counting-primes/Sieve.java
@@ -1,54 +1,31 @@
-/*
- * Source: http://faculty.knox.edu/dbunde/teaching/threadIntro/Sieve.java
- *
- * Alternate prime counting program using the Sieve of Eratosthenes.
- * Original version was written by John M. Hunt (John.Hunt@covenant.edu).
- *
- * Keeps an array of booleans indicating if each value is a prime.
- *
- * Note: this variant has the largest memory overhead O(N). There is no free lunch!
- */
+import java.util.BitSet;
 
 public class Sieve {
     private static final int DEFAULT_UPPER_BOUND = 8000000;
 
-    public static void printSieve(boolean a[]) {
-        for (int i = 2; i < a.length; i++) {
-            if (a[i] == true) {
-                System.out.print(i + ", ");
-            }
-        }
+    private final BitSet sieve;
+
+    public Sieve(int n) {
+        sieve = new BitSet(n);
+        sieve.set(2, n);
     }
 
-    public static int countSieve(boolean a[]) {
-        int count = 0;
-        for (int i = 2; i < a.length; i++) {
-            if (a[i] == true) {
-                count++;
-            }
-        }
-        return count;
+    public void printPrimes() {
+        System.out.println(sieve.toString());
     }
 
-    /*
-     * For each identified prime (true value), cross out all of its multiples.
-     */
-    public static void runSieve(boolean a[]) {
-        for (int i = 2; i < a.length; i++) {
-            if (a[i] == true) {
-                for (int n = i + i; n < a.length; n = n + i) {
-                    a[n] = false;
+    public int getNumberOfPrimes() {
+        return sieve.cardinality();
+    }
+
+    public void findPrimes() {
+        for (int i = 2; i < sieve.length(); i++) {
+            if (sieve.get(i)) {
+                for (int j = i + i; j < sieve.length(); j += i) {
+                    sieve.clear(j);
                 }
             }
         }
-    }
-
-    public static boolean[] initSieve(int size) {
-        boolean nums[] = new boolean[size];
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = true;
-        }
-        return nums;
     }
 
     public static void main(String args[]) {
@@ -57,11 +34,11 @@ public class Sieve {
         if (args.length == 1)
             size = Integer.parseInt(args[0]);
 
-        boolean a[] = initSieve(size);
-        runSieve(a);
-        System.out.println("number of primes = " + countSieve(a));
+        Sieve sieve = new Sieve(size);
+        sieve.findPrimes();
+        System.out.println("Number of primes = " + sieve.getNumberOfPrimes());
 
         if (size <= 100)
-            printSieve(a);
+            sieve.printPrimes();
     }
 }
