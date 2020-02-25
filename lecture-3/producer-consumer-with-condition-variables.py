ff39dbd337d67d285c321ba275145880f6abdcc2
Ervin Varga <e.varga@ieee.org>
2020-02-25 04:35:15 +0100

    Add producer/consumer model examples with Python 3 threads.

---
+++ lecture-3/producer-consumer-with-condition-variables.py
@@ -0,0 +1,61 @@
+from random import randint
+from threading import Condition, RLock, Thread 
+from time import sleep
+
+items = []
+items_lock = RLock()
+buffer_not_empty, buffer_not_full = Condition(items_lock), Condition(items_lock)
+
+
+class Consumer(Thread):
+	def __init__(self, consumer_id):
+		Thread.__init__(self)
+		self._consumer_id = consumer_id
+
+	def consume(self):
+		with items_lock:
+			while not items:
+				print("Consumer %d is waiting..." % self._consumer_id)
+				buffer_not_empty.wait()
+			print("Consumer %d consumed item %d" % (self._consumer_id, items.pop()))
+			buffer_not_full.notify()
+		
+	def run(self):
+		for _ in range(20):
+			sleep(randint(1, 5))
+			self.consume()
+
+			
+class Producer(Thread):
+	def __init__(self, producer_id):
+		Thread.__init__(self)
+		self._producer_id = producer_id
+
+	def produce(self):
+		with items_lock:
+			while len(items) == 10:
+				print("Producer %d is waiting..." % self._producer_id)
+				buffer_not_full.wait()
+			item = randint(1, 100)
+			print("Producer %d has produced item %d" % (self._producer_id, item))
+			items.append(item)
+			buffer_not_empty.notify()
+
+	def run(self):
+		for i in range(20):
+			sleep(randint(1, 5))
+			self.produce()			
+
+
+if __name__ == "__main__":
+        producer1, producer2 = Producer(1), Producer(2)
+        consumer1, consumer2 = Consumer(1), Consumer(2)
+        producer1.start()
+        producer2.start()
+        consumer1.start()
+        consumer2.start()
+        producer1.join()
+        producer2.join()
+        consumer1.join()
+        consumer2.join()
+        print("Simulation is done!")
---
+++ lecture-3/producer-consumer-with-queue.py
@@ -0,0 +1,53 @@
+from queue import Queue
+from random import randint
+from threading import Thread 
+from time import sleep
+
+
+class Consumer(Thread):
+    def __init__(self, consumer_id, queue):
+        Thread.__init__(self)
+        self._consumer_id = consumer_id
+        self._queue = queue
+
+    def consume(self):
+        item = self._queue.get()
+        print("Consumer %d consumed item %d" % (self._consumer_id, item))
+        self._queue.task_done()
+        
+    def run(self):
+        for _ in range(20):
+            sleep(randint(1, 5))
+            self.consume()
+
+            
+class Producer(Thread):
+    def __init__(self, producer_id, queue):
+        Thread.__init__(self)
+        self._producer_id = producer_id
+        self._queue = queue
+
+    def produce(self):
+        item = randint(1, 100)
+        print("Producer %d has produced item %d" % (self._producer_id, item))
+        self._queue.put(item)
+
+    def run(self):
+        for i in range(20):
+            sleep(randint(1, 5))
+            self.produce()            
+
+
+if __name__ == "__main__":
+        queue = Queue()
+        producer1, producer2 = Producer(1, queue), Producer(2, queue)
+        consumer1, consumer2 = Consumer(1, queue), Consumer(2, queue)
+        producer1.start()
+        producer2.start()
+        consumer1.start()
+        consumer2.start()
+        producer1.join()
+        producer2.join()
+        consumer1.join()
+        consumer2.join()
+        print("Simulation is done!")
