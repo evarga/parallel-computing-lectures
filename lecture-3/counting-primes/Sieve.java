667f17739f6136a2cf60802d4373735f4b13db74
Ervin Varga <e.varga@ieee.org>
2020-09-07 17:25:40 +0200

    Improve performance of Sieve.

--- lecture-3/counting-primes/Sieve.java
+++ lecture-3/counting-primes/Sieve.java
@@ -4,10 +4,12 @@ public class Sieve {
     private static final int DEFAULT_UPPER_BOUND = 8000000;
 
     private final BitSet sieve;
+    private final int upperBound;
 
     public Sieve(int n) {
         sieve = new BitSet(n);
         sieve.set(2, n);
+        upperBound = (int) Math.ceil(Math.sqrt(n));
         findPrimes();
     }
 
@@ -20,20 +22,17 @@ public class Sieve {
     }
 
     private void findPrimes() {
-        for (int i = 2; i < sieve.length(); i++) {
-            if (sieve.get(i)) {
-                for (int j = i + i; j < sieve.length(); j += i) {
+        for (int i = 2; i <= upperBound; i++)
+            if (sieve.get(i))
+                for (int j = i * i; j < sieve.length(); j += i)
                     sieve.clear(j);
-                }
-            }
-        }
     }
 
     public static void main(String args[]) {
         int size = DEFAULT_UPPER_BOUND;
 
         if (args.length == 1)
-            size = Integer.parseInt(args[0]);
+            size = Integer.parseInt(args[0]); // Assume size is >= 2.
 
         Sieve sieve = new Sieve(size);
         System.out.println("Number of primes = " + sieve.getNumberOfPrimes());
