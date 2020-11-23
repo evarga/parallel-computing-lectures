fcfd6014743bdf4c8e6ef89ef03f24576d2f374c
Ervin Varga <e.varga@ieee.org>
2020-11-23 17:14:32 +0100

    Fix cosmetic issues in the code.

--- lecture-3/producer-consumer-with-condition-variables.py
+++ lecture-3/producer-consumer-with-condition-variables.py
@@ -10,43 +10,43 @@ buffer_not_empty, buffer_not_full = Condition(items_lock), Condition(items_lock)
 
 
 class Consumer(Thread):
-	def __init__(self, consumer_id):
-		Thread.__init__(self)
-		self._consumer_id = consumer_id
+    def __init__(self, consumer_id):
+        Thread.__init__(self)
+        self._consumer_id = consumer_id
 
-	def consume(self):
-		with items_lock:
-			while not items:
-				print("Consumer %d is waiting..." % self._consumer_id)
-				buffer_not_empty.wait()
-			print("Consumer %d consumed item %d" % (self._consumer_id, items.pop()))
-			buffer_not_full.notify()
-		
-	def run(self):
-		for _ in range(20):
-			sleep(randint(1, 5))
-			self.consume()
+    def consume(self):
+        with items_lock:
+            while not items:
+                print("Consumer %d is waiting..." % self._consumer_id)
+                buffer_not_empty.wait()
+            print("Consumer %d consumed item %d" % (self._consumer_id, items.pop()))
+            buffer_not_full.notify()
 
-			
+    def run(self):
+        for _ in range(20):
+            sleep(randint(1, 5))
+            self.consume()
+
+
 class Producer(Thread):
-	def __init__(self, producer_id):
-		Thread.__init__(self)
-		self._producer_id = producer_id
+    def __init__(self, producer_id):
+        Thread.__init__(self)
+        self._producer_id = producer_id
 
-	def produce(self):
-		with items_lock:
-			while len(items) == 10:
-				print("Producer %d is waiting..." % self._producer_id)
-				buffer_not_full.wait()
-			item = randint(1, 100)
-			print("Producer %d has produced item %d" % (self._producer_id, item))
-			items.append(item)
-			buffer_not_empty.notify()
+    def produce(self):
+        with items_lock:
+            while len(items) == 10:
+                print("Producer %d is waiting..." % self._producer_id)
+                buffer_not_full.wait()
+            item = randint(1, 100)
+            print("Producer %d has produced item %d" % (self._producer_id, item))
+            items.append(item)
+            buffer_not_empty.notify()
 
-	def run(self):
-		for i in range(20):
-			sleep(randint(1, 5))
-			self.produce()			
+    def run(self):
+        for i in range(20):
+            sleep(randint(1, 5))
+            self.produce()
 
 
 if __name__ == "__main__":
